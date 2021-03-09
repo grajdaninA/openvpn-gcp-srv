c4d93d3aa8c33a05fcb2b370ee5c93ac6abdad24
Mikalai Ashuika <ashuikam@gmail.com>
2021-03-09 15:37:52 +0300

    added tfvars, variables files and creds

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone = var.gcp_zone
+  credentials = file(var.path_to_gcp_credentials)
+}
+
+data "google_compute_network" "default" {
+  name = var.gcp_network
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = var.gcp_instance_name
+  machine_type = var.gcp_machine_type
+
+  boot_disk {
+    initialize_params {
+      image = var.gcp_machine_image
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = data.google_compute_network.default.name
+    access_config {
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.gcp_instance_user}:${file(var.instance_ssh_pub_key_path)}"
+  }
+  
+  tags = var.tags
+}
+
+resource "google_compute_firewall" "vpn" {
+  name    = var.gcp_firewall_rule_name
+  network = data.google_compute_network.default.name
+
+  allow {
+    protocol = var.firewall_protocol
+    ports    = var.firewall_ports
+  }
+
+  source_tags = var.firewall_rule_source_tags
+  target_tags = [var.tags[1]]
+}
+
---
+++ variables.tf
@@ -0,0 +1,85 @@
+variable "instance_ssh_pub_key_path" {
+  type = string
+  description = "The path to SSH public key to instances access"
+}
+
+variable "path_to_private_ssh_key" {
+  type = string
+  description = "The path to SSH private key to instances access"
+}
+
+variable "path_to_gcp_credentials" {
+  type = string
+  description = "The path to GCP credential file with the JSON format"
+}
+
+variable "gcp_zone" {
+  type = string
+  description = "The Google Computer Engine zone"
+}
+
+variable "gcp_region" {
+  type = string
+  description = "The Google Computer Engine region"
+}
+
+variable "gcp_project" {
+  type = string
+  description = "The GCP Project"
+}
+
+variable "gcp_instance_name" {
+  type = string
+  description = "The GCE instance name"
+}
+
+variable "gcp_machine_type" {
+  type = string
+  description = "The instances configuration"
+}
+
+variable "gcp_machine_image" {
+  type = string
+  description = "The GCE OS image"
+}
+
+variable "gcp_instance_user" {
+  type = string
+  description = "The instance user name"
+}
+
+variable "tags" {
+  type = list(string)
+  description = "The tags to mark resources"
+}
+
+variable "gcp_network" {
+  type = string
+  description = "The GCE network name"
+  default = "default"
+}
+
+variable "gcp_firewall_rule_name" {
+  type = string
+  description = "The GCP VPC network firewall rule name"
+}
+
+variable "firewall_protocol" {
+  type = string
+  description = "The firewall rule protocol"
+  default = "udp"
+}
+
+variable "firewall_ports" {
+  type = list(string)
+  description = "The list of ports to access"
+  default = ["1194"]
+}
+
+variable "firewall_rule_source_tags" {
+  type = list(string)
+  description = "The list of firewall rule source tags"
+  default = ["proxy"]
+}
+
+
